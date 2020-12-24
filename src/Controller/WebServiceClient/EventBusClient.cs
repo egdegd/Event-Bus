92964098a66710dc77286d878d942333c7f90f5e
Григорий Эмдин <egd3700@mail.ru>
2020-12-24 21:08:36 +0300

    add controller for event bus and testServer

---
+++ src/Controller/WebServiceClient/EventBusClient.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Utils;
+using Newtonsoft.Json;
+using System.Configuration;
+
+namespace WebServiceClient
+{
+    public class EventBusClient
+    {
+        static readonly HttpClient client = new HttpClient();
+        static readonly string eventBusHost = ConfigurationManager.AppSettings["EventBusHost"];
+        public HttpResponseMessage AddMessage(string from, string recipient, string text)
+        {
+            try
+            {
+                var msg = new Message
+                {
+                    From = from,
+                    To = recipient,
+                    Text = text
+                };
+
+                var serializedObject = JsonConvert.SerializeObject(msg);
+                var content = new StringContent(serializedObject, Encoding.UTF8, "application/json");
+                var response = client.PostAsync($"{eventBusHost}/addmsg", content).Result;
+                return response;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Request error", e);
+                throw;
+            }
+        }
+
+        public string SendMessage(string name)
+        {
+            try
+            {
+                var response = client.GetAsync($"{eventBusHost}/sendmsg?name={name}").Result;
+                var result = response.Content.ReadAsStringAsync().Result;
+                return result;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Request error", e);
+                throw;
+            }
+
+        }
+
+        public string SendEvent(string name)
+        {
+            try
+            {
+                var response = client.GetAsync($"{eventBusHost}/sendevent?name={name}").Result;
+                var result = response.Content.ReadAsStringAsync().Result;
+                return result;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Request error", e);
+                throw;
+            }
+        }
+
+        public string Publish(string organizer, string type, string description)
+        {
+            try
+            {
+                Event e = new Event
+                {
+                    Type = type,
+                    Description = description,
+                    Organizer = organizer
+                };
+
+                var serializedObject = JsonConvert.SerializeObject(e);
+                var content = new StringContent(serializedObject, Encoding.UTF8, "application/json");
+                var response = client.PostAsync($"{eventBusHost}/publish", content).Result;
+                var result = response.Content.ReadAsStringAsync().Result;
+                return result;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Request error", e);
+                throw;
+            }
+        }
+
+        public string Subscribe(string name, string type)
+        {
+            try
+            {
+                Pair p = new Pair
+                {
+                    First = name,
+                    Second = type
+                };
+                var serializedObject = JsonConvert.SerializeObject(p);
+                var content = new StringContent(serializedObject, Encoding.UTF8, "application/json");
+                var response = client.PostAsync($"{eventBusHost}/subscribe", content).Result;
+                var result = response.Content.ReadAsStringAsync().Result;
+                return result;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Request error", e);
+                throw;
+            }
+        }
+
+        public string Unsubscribe(string name, string type)
+        {
+            try
+            {
+                Pair p = new Pair
+                {
+                    First = name,
+                    Second = type
+                };
+                var serializedObject = JsonConvert.SerializeObject(p);
+                var content = new StringContent(serializedObject, Encoding.UTF8, "application/json");
+                var response = client.PostAsync($"{eventBusHost}/unsubscribe", content).Result;
+                var result = response.Content.ReadAsStringAsync().Result;
+                return result;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Request error", e);
+                throw;
+            }
+        }
+
+        public HttpResponseMessage AddMessageInDB(string from, string recipient, string text)
+        {
+            try
+            {
+                var msg = new Message
+                {
+                    From = from,
+                    To = recipient,
+                    Text = text
+                };
+
+                var serializedObject = JsonConvert.SerializeObject(msg);
+                var content = new StringContent(serializedObject, Encoding.UTF8, "application/json");
+                var response = client.PostAsync($"{eventBusHost}/addmsgindb", content).Result;
+                return response;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Request error", e);
+                throw;
+            }
+        }
+    }
+}
---
+++ src/Controller/WebServiceClient/SampleServiceClient.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace WebServiceClient
+{
+    public class SampleServiceClient
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        public string SendMessage(string localhost, string fromName, string toName, string text)
+        {
+            string url = $"http://localhost:{localhost}/api/{fromName}/sendmsg?recipient={toName}&text={text}";
+            
+            try
+            {
+                var response = client.GetAsync(url).Result;
+                Logger.Info("Client sent request: " + url);
+
+                var result = response.Content.ReadAsStringAsync().Result;
+                Logger.Info("Client received response: " + result);
+                return result;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Request error", e);
+                throw;
+            }
+        }
+
+        public string Subscribe(string localhost, string name, string type)
+        {
+            string url = $"http://localhost:{localhost}/api/{name}/subscribe?type={type}";
+
+            try
+            {
+                var response = client.GetAsync(url).Result;
+                Logger.Info("Client sent request: " + url);
+
+                var result = response.Content.ReadAsStringAsync().Result;
+                Logger.Info("Client received response: " + result);
+                return result;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Request error", e);
+                throw;
+            }
+        }
+
+        public string Unsubscribe(string localhost, string name, string type)
+        {
+            string url = $"http://localhost:{localhost}/api/{name}/unsubscribe?type={type}";
+
+            try
+            {
+                var response = client.GetAsync(url).Result;
+                Logger.Info("Client sent request: " + url);
+
+                var result = response.Content.ReadAsStringAsync().Result;
+                Logger.Info("Client received response: " + result);
+                return result;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Request error", e);
+                throw;
+            }
+        }
+
+        public string Publish(string localhost, string name, string type, string description)
+        {
+            string url = $"http://localhost:{localhost}/api/{name}/publishevent?type={type}&description={description}";
+
+            try
+            {
+                var response = client.GetAsync(url).Result;
+                Logger.Info("Client sent request: " + url);
+
+                var result = response.Content.ReadAsStringAsync().Result;
+                Logger.Info("Client received response: " + result);
+                return result;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Request error", e);
+                throw;
+            }
+        }
+
+    }
+}
